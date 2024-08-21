28c2a123447741d3631d000d1150e673a4259236
Kang IWon <esfne4727@naver.com>
2024-08-21 17:29:23 +0900

    기본 구조 세팅

---
+++ src/main/java/com/sparta/spring/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.sparta.spring.service;
+
+import com.sparta.spring.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+}
